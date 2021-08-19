d61be37b05edc3f8bb0682e2bfb01887d7f8201e
fenegjixuchui <853074174@qq.com>
2021-08-19 14:14:43 +0800

    添加装饰器模式

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
 /*
  * @Author: ALex
  * @Date: 2021-08-19 11:49:23
- * @LastEditTime: 2021-08-19 14:02:33
+ * @LastEditTime: 2021-08-19 14:12:07
  * @LastEditors: Please set LastEditors
  * @Description: 基于customize和react-app-rewired的定制化配置文件
  * @FilePath: /react-konwledge/config-overrides.js
@@ -10,11 +10,13 @@
 //从customize-cra引入一些相关的方法
 const { override,
     addLessLoader,
-    fixBabelImports
+    fixBabelImports,
+    addDecoratorsLegacy
 } = require('customize-cra')
 
 const modifyVars = require('./lessVars')
 module.exports = override(
+    addDecoratorsLegacy(),
     addLessLoader({
         javascriptEnabled: true,
         modifyVars
--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@
 import React, { Component } from 'react'
 
 import { Button } from 'antd'
+
+const testHOC = (WrapedComponent) =>{
+    return class HOCComponent extends Component {
+        render(){
+            return (
+                <>
+                <WrapedComponent />
+                <div>这是高阶组建里的信息</div>
+                </>
+            )
+        }
+    }
+}
+@testHOC
 class App extends Component {
     render() {
         return (
